fc61825cfd9a459e556cd0b85f7a31c9c3e1def3
Yaro <chromaticserpent@gmail.com>
2023-10-24 19:14:29 +1300

    rework updateQuality method to meet the requirements, fix tests to pass

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,5 +1,7 @@
 
 import { Item } from './item'
+import { ItemToCategoryMap } from './item-to-category-map';
+import { ItemCategory } from './item-category-enum';
 
 export class GildedRose {
   items: Array<Item>;
@@ -9,52 +11,60 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
+      for (let i = 0; i < this.items.length; i++) {
+        const item = this.items[i]
+        const itemCategory = ItemToCategoryMap.get(this.items[i].name)
+
+        switch(itemCategory) {
+          // items like "Aged Brie" appreciate in quality by 1 each day
+          case ItemCategory.APPRECIATING_ITEMS:
+            this.increaseQuality(item)
+            break;
+
+          // items like "+5 Dexterity Vest" and "Elixir of the Mongoose" depreciate in quality by 1 each day
+          case ItemCategory.DEPRECIATING_ITEMS:
+            this.decreaseQuality(item)
+            break;
+
+          // items like "Sulfuras, Hand of Ragnaros" have their quality unaltered at 80 no matter what
+          case ItemCategory.ASSETS:
+            break;
+
+          // special items like "Backstage passes to a TAFKAL80ETC concert" gradually appreciate in quality 
+          // until the date of the concert, and then have 0 quality
+          case ItemCategory.SPECIAL_ITEMS:
+            if(item.sellIn > 10){
+              this.increaseQuality(item)
             }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
+            if(item.sellIn <= 10 && item.sellIn > 5){
+              this.increaseQuality(item, 2)
             }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
+            if(item.sellIn <= 5 && item.sellIn > 0){
+              this.increaseQuality(item, 3)
             }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
+            if(item.sellIn <= 0){
+              item.quality = 0
+            }
+            break;
+
+          // conjured items depreciate in quality twice as fast
+          case ItemCategory.CONJURED_ITEMS:
+            this.decreaseQuality(item, 2)
+            break;
         }
+        item.sellIn = item.sellIn - 1
       }
+  
+      return this.items;
     }
 
-    return this.items;
+  private increaseQuality(item: Item, increaseRate: number = 1) {
+    const newQualityValue = item.quality + increaseRate
+    item.quality = newQualityValue >= 50 ? 50 : newQualityValue
+  }
+
+  private decreaseQuality(item: Item, decreaseRate: number = 1) {
+    const newQualityValue = item.quality - decreaseRate
+    item.quality = newQualityValue <= 0 ? 0 : newQualityValue
   }
 }
--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -2,10 +2,9 @@ import { GildedRose } from '@/gilded-rose';
 import { Item } from '@/item';
 
 describe('Gilded Rose', () => {
-  // Confused about the purpose of this test, so I'm going to skip it
-  it.skip('should foo', () => {
+  it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
   });
 });
